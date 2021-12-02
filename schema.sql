e4b2210895d44213e5d3fe53b8e86b1f6e777500
OlgaMartazova <olyamarta34@gmail.com>
2021-12-02 13:17:41 +0300

    gitignore added

--- schema.sql
+++ schema.sql
@@ -1,38 +1,41 @@
-create table users (
-    user_id serial primary key,
-    first_name varchar(30) not null,
-    last_name varchar(30) not null,
-    email varchar(100) not null unique,
-    password_hash varchar(100) not null,
-    role integer not null,
-    avatar_id integer,
-    foreign key (avatar_id) references avatar_file(avatar_file_id)
+create table users
+(
+    user_id       serial
+        constraint users_pkey
+            primary key,
+    first_name    varchar(30)  not null,
+    last_name     varchar(30)  not null,
+    email         varchar(100) not null
+        constraint users_email_key
+            unique,
+    password_hash integer      not null,
+    role          varchar(30)  not null,
+    avatar_id     integer
+        constraint users_avatar_id_fkey
+            references avatar_file
 );
 
-create table avatar_file (
-    avatar_file_id serial primary key,
+create table avatar_file
+(
+    avatar_file_id     serial
+        constraint avatar_file_pkey
+            primary key,
     original_file_name varchar(100),
-    storage_file_name varchar(100) not null,
-    size bigint not null,
-    type varchar(100)
+    storage_file_name  varchar(100) not null,
+    size               bigint       not null,
+    type               varchar(100)
 );
 
-create table posts (
-    post_id serial primary key,
-    author_id int not null,
-    foreign key (author_id) references users(user_id),
-    created_at timestamp not null,
-    content varchar(1000) not null
+create table lessons
+(
+    lesson_id  integer default nextval('posts_post_id_seq'::regclass) not null
+        constraint posts_pkey
+            primary key,
+    author_id  integer                                                not null
+        constraint posts_author_id_fkey
+            references users,
+    created_at timestamp                                              not null,
+    content    varchar(1000)                                          not null,
+    subject    varchar(30),
+    for_whom   varchar(30)
 );
-
-create table attached_file (
-    file_id serial primary key,
-    original_file_name varchar(100),
-    storage_file_name varchar(100) not null,
-    size bigint not null,
-    type varchar(100),
-    post_id int not null,
-    foreign key (post_id) references posts(post_id)
-);
-
-alter table users alter column password_hash type integer using password_hash::integer;
